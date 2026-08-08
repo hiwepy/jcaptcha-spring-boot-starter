d5c7dfd0b7888b8b4b456c9b78d6719c2bfacd24
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:44:59 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/com/octo/captcha/spring/boot/filter/image/ImageCaptchaFilter.java
+++ src/main/java/com/octo/captcha/spring/boot/filter/image/ImageCaptchaFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -21,7 +21,7 @@ import jakarta.servlet.ServletException;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 
 import com.octo.captcha.module.jmx.JMXRegistrationHelper;
