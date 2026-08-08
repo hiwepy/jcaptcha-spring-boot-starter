34e17222c51326f8993f23378cd9692342989fe1
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 02:14:28 +0800

    docs: propagate Javadoc improvements from 4.1.x

---
+++ src/main/java/com/octo/captcha/spring/boot/exception/CaptchaTimeoutException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017, hiwepy (https://github.com/easy-4-java).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.octo.captcha.spring.boot.exception;
+
+/**
+ * 验证码已经过期异常
+ * @author [@Loong Wan](https://github.com/loong10k)
+ */
+@SuppressWarnings("serial")
+public class CaptchaTimeoutException extends Exception {
+
+	public CaptchaTimeoutException() {
+        super("captcha expired");
+    }
+
+    public CaptchaTimeoutException(String message) {
+        super(message);
+    }
+    
+}
